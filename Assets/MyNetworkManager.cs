98a1a1c782d34c79a453c005d63603607501e14a
Alice Robinson <valkrysa@gmail.com>
2017-08-22 22:44:07 -0700

    Through lecture 288, Network Manager and inheritance/overrides

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+    public override void OnStartHost () {
+        Debug.Log("Host started at " + GetCurrentDateTime());
+    }
+
+    public void MyStartHost () {
+        Debug.Log("Starting host at " + GetCurrentDateTime());
+        StartHost();
+    }
+
+    private string GetCurrentDateTime () {
+        DateTime now = DateTime.Now;
+        string nowFormatted = now.ToString("yyyy-MM-dd HH:mm:ss");
+
+        return nowFormatted;
+    }
+}
